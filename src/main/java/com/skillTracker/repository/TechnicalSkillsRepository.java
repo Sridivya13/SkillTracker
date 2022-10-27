33cb8b0ce93a7f1a5dac842896e0b6c60442812b
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:50:59 +0530

    Update TechnicalSkillsRepository.java

--- src/main/java/com/skillTracker/repository/TechnicalSkillsRepository.java
+++ src/main/java/com/skillTracker/repository/TechnicalSkillsRepository.java
@@ -1,4 +1,20 @@
 package com.skillTracker.repository;
 
-public class TechnicalSkillsRepository {
+import com.skillTracker.entity.TechnicalSkillsEntity;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TechnicalSkillsRepository extends MongoRepository<TechnicalSkillsEntity, Integer> {
+
+    Optional<TechnicalSkillsEntity> findByUserId(Integer userId);
+
+    @Query("{ criteriaValue : { $gt: ?10 } }")
+    List<TechnicalSkillsEntity> findBySkill(String criteriaValue);
+
 }
