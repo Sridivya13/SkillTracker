e6afb01dc771f59e85005f428962a5266fac2263
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:51:23 +0530

    Update UserRepository.java

--- src/main/java/com/skillTracker/repository/UserRepository.java
+++ src/main/java/com/skillTracker/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.skillTracker.repository;
 
-public class UserRepository {
+import com.skillTracker.entity.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, Integer> {
+
+   // List<UserEntity> findByUserId(Integer userId);
+
+    List<UserEntity> findByUserNameLike(String userName);
+
+    List<UserEntity> findByAssociateId(String associateId);
 }
