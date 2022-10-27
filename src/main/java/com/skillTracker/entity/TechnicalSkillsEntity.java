567eb8386a3ca5a88f03e3482d518fc59a9093a2
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:46:59 +0530

    Update TechnicalSkillsEntity.java

--- src/main/java/com/skillTracker/entity/TechnicalSkillsEntity.java
+++ src/main/java/com/skillTracker/entity/TechnicalSkillsEntity.java
@@ -1,4 +1,36 @@
 package com.skillTracker.entity;
 
-public class TechnicalSkillsEntity {
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Document(collection = "technicalSkills")
+public class TechnicalSkillsEntity implements Serializable {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "technicalSkills";
+    private static final long serialVersionUID = 955728933773177564L;
+    @Id
+    private Integer technicalSkillsId;
+    private Integer userId;
+    private Integer hTMLCssJavascript;
+    private Integer angular;
+    private Integer react;
+    private Integer spring;
+    private Integer restful;
+    private Integer hibernate;
+    private Integer git;
+    private Integer docker;
+    private Integer jenkins;
+    private Integer aws;
 }
