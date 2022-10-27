27e1cec541dfb60f45fa7e1496afbed957b071aa
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:52:48 +0530

    Update AddProfile.java

--- src/main/java/com/skillTracker/service/AddProfile.java
+++ src/main/java/com/skillTracker/service/AddProfile.java
@@ -1,4 +1,13 @@
 package com.skillTracker.service;
 
+import com.skillTracker.model.*;
+import org.springframework.http.ResponseEntity;
+
 public interface AddProfile {
+    ResponseEntity<UserResponse> registerUser(User user);
+
+    ResponseEntity<UpdateResponse> updateProfile(Integer userId, Skills skills);
+
+    ResponseEntity<SearchResponse> searchProfile(String criteria, String criteriaValue);
 }
+
