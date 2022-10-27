41d88807065bfeeba30860568402de31078d9aa1
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:50:39 +0530

    Update NonTechnicalSkillsRepository.java

--- src/main/java/com/skillTracker/repository/NonTechnicalSkillsRepository.java
+++ src/main/java/com/skillTracker/repository/NonTechnicalSkillsRepository.java
@@ -1,4 +1,20 @@
 package com.skillTracker.repository;
 
-public class NonTechnicalSkillsRepository {
+import com.skillTracker.entity.NonTechnicalSkillsEntity;
+import com.skillTracker.entity.TechnicalSkillsEntity;
+import com.skillTracker.entity.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NonTechnicalSkillsRepository extends MongoRepository<NonTechnicalSkillsEntity, Integer> {
+
+    Optional<NonTechnicalSkillsEntity> findByUserId(Integer userId);
+
+    @Query("{ criteriaValue : { $gt: ?10 } }")
+    List<NonTechnicalSkillsEntity> findBySkill(String criteriaValue);
 }
