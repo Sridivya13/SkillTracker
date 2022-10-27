2f1c7373093355da674c1c05ef98c1f524bff01e
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:50:08 +0530

    Update UserResponse.java

--- src/main/java/com/skillTracker/model/UserResponse.java
+++ src/main/java/com/skillTracker/model/UserResponse.java
@@ -1,4 +1,22 @@
 package com.skillTracker.model;
 
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class UserResponse {
+
+    private int userId;
+    private String messageType;
+    private HttpStatus messageCode;
+    private String message;
+
+
 }
+
