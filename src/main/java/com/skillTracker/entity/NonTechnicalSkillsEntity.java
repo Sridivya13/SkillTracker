77c1a7537a461bf12cce09a564be290570b88e6e
Sridivya13 <78948279+Sridivya13@users.noreply.github.com>
2022-10-27 17:46:36 +0530

    Update NonTechnicalSkillsEntity.java

--- src/main/java/com/skillTracker/entity/NonTechnicalSkillsEntity.java
+++ src/main/java/com/skillTracker/entity/NonTechnicalSkillsEntity.java
@@ -1,4 +1,29 @@
 package com.skillTracker.entity;
 
-public class NonTechnicalSkillsEntity {
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Document(collection = "nonTechnicalSkills")
+public class NonTechnicalSkillsEntity implements Serializable {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "nonTechnicalSkills";
+    private static final long serialVersionUID = 955728933773177564L;
+    @Id
+    private Integer nonTechnicalSkillsId;
+    private Integer userId;
+    private Integer spoken;
+    private Integer communication;
+    private Integer aptitude;
 }
